87ed5094031ec6c392960c64368b03f945234f70
onurabdulaji <onurabdulaji@gmail.com>
2025-04-09 17:49:50 +0200

    Fluent Api for Hero

---
+++ Infrastructure/PortfolioV2.Persistence/Configurations/HeroConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PortfolioV2.Domain.Entities;
+
+namespace PortfolioV2.Persistence.Configurations;
+
+public class HeroConfiguration : BaseConfiguration<Hero>
+{
+    public override void Configure(EntityTypeBuilder<Hero> builder)
+    {
+        base.Configure(builder);
+    }
+}
